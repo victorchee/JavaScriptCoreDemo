170fa7f7af7bd6c5e9a256160d2ef9e8ebc941cb
Victor Chee <victor.qhj@gmail.com>
2015-12-15 09:34:32 +0800

    add project

---
+++ JavaScriptCoreDemo/JavaScriptCoreDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JavaScriptCoreDemo
+//
+//  Created by qihaijun on 12/15/15.
+//  Copyright © 2015 VictorChee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
